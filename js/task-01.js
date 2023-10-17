7276d036da909fbd1aebe77089a8fd3b1cbbd1a8
Artur Didur <didur.art@gmail.com>
2023-10-17 11:41:59 +0200

    fix task 1
    used DOM navigation

--- js/task-01.js
+++ js/task-01.js
@@ -5,7 +5,7 @@ const categoryItems = document.querySelectorAll("#categories .item");
 let message = `Number of categories: ${categoryItems.length}\n\n`;
 
 categoryItems.forEach(categoryItem => {
-  const categoryH2 = categoryItem.querySelector("h2").textContent;
+  const categoryH2 = categoryItem.firstElementChild.textContent;
   const categoryLi = categoryItem.querySelectorAll("ul li");
   message += `Category: ${categoryH2}\n` + `Elements: ${categoryLi.length}\n\n`;
 });
